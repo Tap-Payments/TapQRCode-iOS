ba71dd97220fce461474904f0eb502d850c091de
Tap Payments <o.rabie@tap.company>
2020-03-18 10:56:06 +0200

    1- Documentation

--- TapQRCode-iOS/Core/Public/TapQRCodeScanner/TapQRCodeScanner.swift
+++ TapQRCode-iOS/Core/Public/TapQRCodeScanner/TapQRCodeScanner.swift
@@ -41,25 +41,34 @@ import QRCodeReader
         return UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
-    
+    /**
+        Interface to start the scanner if all requirments are met, inside a bounvded uiview
+     - Parameter holdingView: The subview the caller wants to show  the scanner in
+     - Parameter shouldHideUponScanning: if true, then the scanner will dismiss itself after reading a code
+     */
     @objc public class func scanInline(inside holdingView:UIView, shouldHideUponScanning:Bool) {
         
+        // First of all we need to check we can start the scanner
         guard canStartScanner(), !reader.isRunning else {
             print("Camera permission is required")
             return
         }
         
         if let _ = previewView{
+            // Defensive code to remove any preview view before to make sure memory is always clean
             DispatchQueue.main.async {
                 previewView?.removeFromSuperview()
             }
         }
-        reader.previewLayer
+        
+        // Assing the new frame given to the preview scanner
         previewView = QRCodeReaderView(frame: CGRect(x: 0, y: 0, width: holdingView.frame.width, height: holdingView.frame.height))
         
+        // This will setup the inline scanner with the default attribtes and assigns the camera reader to the preview frame
         setupInlineQRScanner()
         
         DispatchQueue.main.async {
+            // Add the preview frame to the passed subview
             holdingView.addSubview(previewView!)
         }
         
@@ -71,15 +80,21 @@ import QRCodeReader
         }
         
         
-
+        // All good!
         reader.startScanning()
     }
     
+    /**
+     Interface to remove and stop the inline scanner. Can be used only if the caller needs tto handle himself dismissing the inline scanner
+     */
     @objc public class func stopInlineScanning() {
         previewView?.removeFromSuperview()
         reader.stopScanning()
     }
     
+    /**
+     This is a private helper method that will configure the qr code scanner view, it will dismiss all the buttons like flip camera or torch as in inline. Also defines the default area of interest
+     */
     private class func setupInlineQRScanner() {
         previewView?.setupComponents(with: QRCodeReaderViewControllerBuilder {
           $0.reader                 = reader
