5a03eba3295adeb1d2d526f5df54ceb5c4142753
Tap Payments <o.rabie@tap.company>
2020-03-18 08:47:28 +0200

    1- Added the public interface for scan can start intterfface. 2- Added privately all helper methods

--- TapQRCode-iOS/Core/Public/TapQRCodeScanner/TapQRCodeScanner.swift
+++ TapQRCode-iOS/Core/Public/TapQRCodeScanner/TapQRCodeScanner.swift
@@ -7,10 +7,33 @@
 //
 
 import Foundation
+import class AVFoundation.AVCaptureDevice
+import class UIKit.UIImagePickerController
 
 /// This provides the public interface for the caller to the Tap QR code Scanner
 @objc public class TapQRCodeScanner:NSObject {
     
+    /**
+     The method checks for camera existance and permissions.
+     - Returns: True if the camera exists in the device and the user granted the camera usage permission for the app. False othweise
+     **/
+    @objc public class func canStartScanner()->Bool {
+        return checkCameraExists() && checkCameraPermission()
+    }
     
+    /**
+    The method checks for camera permission.
+    - Returns: True if the user granted the camera usage permission for the app. False othweise
+    **/
+    private class func checkCameraPermission()->Bool {
+        return AVCaptureDevice.authorizationStatus(for: .video) ==  .authorized
+    }
+    
+    /**The method checks for camera permission.
+     - Returns: True if the camera exists in the device. False othweise
+     **/
+    private class func checkCameraExists()->Bool {
+        return UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
     
 }
